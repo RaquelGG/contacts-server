22d74b5abcc5fbddfc55e570fcd9fcbcf4168af5
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-16 01:20:05 +0100

    add format validation to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,9 @@ class Contact < ApplicationRecord
   has_many :change, dependent: :destroy
   validates_presence_of :name, :surname, :email, :tel
   validates :email, uniqueness: true
+
+  # tel regex from https://regexr.com/3c53v
+  validates_format_of :tel, :with => /\A\+*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.0-9]*$\z/
+
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Contact, type: :model do
   it { should have_many(:change).dependent(:destroy) }
@@ -6,5 +6,4 @@ RSpec.describe Contact, type: :model do
   it { should validate_presence_of(:surname) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:tel) }
-  
 end

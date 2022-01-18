2b80aa4cf1eefba28ffeb2386b34c58d9995b9e7
RaquelGG <57768789+RaquelGG@users.noreply.github.com>
2022-01-18 12:11:56 +0100

    remove lenght limit

--- db/migrate/1_create_contacts.rb
+++ db/migrate/1_create_contacts.rb
@@ -1,10 +1,10 @@
 class CreateContacts < ActiveRecord::Migration[7.0]
   def change
     create_table :contacts do |t|
-      t.string :name, limit: 45, null: false
-      t.string :surname, limit: 45, null: false
-      t.string :email, unique: true, limit: 45, null: false
-      t.string :tel, limit: 45, null: false
+      t.string :name, null: false
+      t.string :surname, null: false
+      t.string :email, unique: true, null: false
+      t.string :tel, null: false
     end
   end
 end
